39e6b6dc73002bb22f2f311f8faccd21064418f1
damongeronis <damongeronis@gmail.com>
2018-07-19 14:51:37 -0500

    Changes in names were made

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-apps',
+  template: `<events-list></events-list>
+  `,
+  
+})
+export class EventsAppComponent {
+  title = 'app';
+}
