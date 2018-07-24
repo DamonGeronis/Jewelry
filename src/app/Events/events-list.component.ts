4bf16d638041157a05b532f7f8aebbb41fa3a757
damongeronis <damongeronis@gmail.com>
2018-07-24 11:02:11 -0500

    more products added
    ngFor testing

--- src/app/Events/events-list.component.ts
+++ src/app/Events/events-list.component.ts
@@ -2,20 +2,51 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'events-list',
-    templateUrl: './events-list.component.html'
+    templateUrl: './events-list.component.html',
+    styles: [`
+     .thumbnail { min-height: 210px; }
+     .pad-left {margin-left: 10px; }
+     .well div {color: #bbb; }
+    `]
 })
-export class EventsListComponent{
-    event = {
+export class EventsListComponent {
+    events = [{
         id: 1,
         name: 'Anillo',
         date: '9/26/2018',
         cost: 600,
         price: 1200,
-        imageUrl:'/assets/images/ring.jpg',
+        imageUrl: '/assets/images/ring.jpg',
         specifications: {
-            weight : 1.2,
+            weight: 1.2,
             kilatage: 14,
             metal: 'Oro'
         }
+    },
+    {
+        id: 1,
+        name: 'Medalla',
+        date: '9/26/2018',
+        cost: 800,
+        price: 1600,
+        imageUrl:'/assets/images/ring.jpg',
+        specifications: {
+            weight : 3,
+            kilatage: 14,
+            metal: 'Oro'
+        }
+    },{
+        id: 1,
+        name: 'Huggies',
+        date: '9/26/2018',
+        cost: 400,
+        price: 1800,
+        imageUrl:'/assets/images/ring.jpg',
+        specifications: {
+            weight : 2.2,
+            kilatage: 14,
+            metal: 'Florentino'
+        }
     }
+    ]
 }
